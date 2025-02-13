05945ad62f82d3c1b93b48e1fb5192eda2b43a1b
vinod-kumar1 <vinu22149@gmail.com>
2025-02-13 08:03:38 +0530

    working example

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let { v4 } = require("uuid");
 // Create WebSocket server on port 8080
 const wss = new WebSocket.Server({ port: 8080 });
 // Connection event handler
-let publicClients = [];
+let publicClients = new Set();
 
 wss.on('connection', (ws) => {
   let clientId;
@@ -31,14 +31,16 @@ wss.on('connection', (ws) => {
         console.log("New client connected: " + msg.id);
         clientId = v4(msg.id);
         ws.client = clientId;
-        publicClients.push(clientId)
-        ws.send(JSON.stringify({ type: "newClient", client: publicClients }));
+        publicClients.add(clientId)
+        wss.clients.forEach(ws => ws.send(JSON.stringify({ type: "newClient", client: Array.from(publicClients) })));
         break;
 
       case "message":
         console.log("received a message from client", msg.client, msg.message);
         !msg.client ? console.log(ws.clientName, ':', msg.message) : sendMessageToClient(msg);
         break;
+      case "close":
+        publicClients.delete(msg.client);
 
       default:
         console.log('Unknown message type:', msg.type);
@@ -59,11 +61,11 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-function sendMessageToClient({ message, client }) {
+function sendMessageToClient({ message, client, name }) {
   wss.clients.forEach(cl => {
-    console.log(client, cl.client, "nc-l");
+    // console.log(client, cl.client, "nc-l");
     if (cl.client == client) {
-      cl.send(JSON.stringify({ type: "message", message: message }));
+      cl.send(JSON.stringify({ type: "message", message: message, name: name }));
     }
   });
 
